1e2e02198b39a938bac109d14499421c757e41dd
mika noda <mi.p0v0q.ka@gmail.com>
2022-10-10 20:59:52 +0900

    mainImg・sub-contents　SCSS HTML 修正 写真 JS追加

---
+++ script.js
@@ -0,0 +1,50 @@
+$(function () {
+
+    $(window).scroll(function () {
+        $('.intro-ph').each(function () {
+            var position = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > position - windowHeight + 200) {
+                $(function () {
+                    $('.intro-ph').each(function (i) {
+                        $(this).delay(i * 800).queue(function () {
+                            $(this).addClass('fadein');
+                        });
+                    });
+                });
+            }
+        });
+    });
+
+
+
+    $(window).scroll(function () {
+        $('.intro-text01').each(function () {
+            let position = $(this).offset().top;
+            let scroll = $(window).scrollTop();
+            let windowHeight = $(window).height();
+            if (scroll > position - windowHeight + 200) {
+                $(function () {
+                    $('.intro-text01').each(function (i) {
+                        $(this).delay(i * 300).queue(function () {
+                            $(this).addClass('fadein'),
+                            setTimeout(function() {
+                                $('.intro-text01').removeClass('fadein');
+                            }, 1500);
+                        });
+                    });
+                });
+            }
+        });
+       
+    });
+    
+
+
+
+
+
+
+
+})
