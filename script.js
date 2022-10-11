143e5f11bc2a7d9de40554bcf66c3c89865e45f3
mika noda <mi.p0v0q.ka@gmail.com>
2022-10-11 17:01:43 +0900

    HTML SASS TOPへ戻る　レスポンシブ対応

--- script.js
+++ script.js
@@ -29,21 +29,45 @@ $(function () {
                     $('.intro-text01').each(function (i) {
                         $(this).delay(i * 300).queue(function () {
                             $(this).addClass('fadein'),
-                            setTimeout(function() {
-                                $('.intro-text01').removeClass('fadein');
-                            }, 1500);
+                                setTimeout(function () {
+                                    $('.intro-text01').removeClass('fadein');
+                                }, 1500);
                         });
                     });
                 });
             }
+
+            $(window).on('scroll', function () {
+
+                // jQuery(document).height()・・・ページ全体の高さ
+                // jQuery(window).height()・・・表示されているウインドウの高さ
+                // jQuery(window).scrollTop()・・・（ページの一番上からの）スクロールの量
+                let scrollHeight = $(document).height();
+                let scrollPosition = $(window).height() + $(window).scrollTop();
+                let footHeight = $('#footer-content').innerHeight();
+
+                // ページ全体の高さ - ページのスクロール量 <= ナビを止めたい高さ
+                if (scrollHeight - scrollPosition <= footHeight) {
+                    $('#page_top').css({
+                        position: 'absolute',
+                        bottom: footHeight
+                    })
+                } else {
+                    $('#page_top').css({
+                        "position": 'fixed',
+                        'bottom': '10px',
+                    })
+                }
+            })
+            // クリックしたらTOPに戻る
+            $('#page_top').on('click', function () {
+                $('body, html').animate({ scrollTop: 0 }, 500);
+                return false;
+            });
+
         });
-       
+
     });
-    
-
-
-
-
 
 
 
